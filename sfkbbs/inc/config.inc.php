e056bcaf201521c72b1c44d270af256406d6affc
wsm1025 <64190019+wsm1025@users.noreply.github.com>
2021-08-12 15:46:59 +0800

    Update config.inc.php

--- sfkbbs/inc/config.inc.php
+++ sfkbbs/inc/config.inc.php
@@ -3,11 +3,11 @@ date_default_timezone_set('Asia/Shanghai');
 define('IN_WSM',true);
 session_start();
 header('Content-type:text/html;charset=utf-8');
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASSWORD','980703');
-define('DB_DATABASE','sfkbbs');
-define('DB_PORT',3306);
+define('DB_HOST','x x x');
+define('DB_USER','x x x');
+define('DB_PASSWORD','x x x');
+define('DB_DATABASE','x x x');
+define('DB_PORT',x x x);
 define('IMG_USER','https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1851241116,346865000&fm=11&gp=0.jpg');
 //定义全局变量为其他php提供参数
 //绝对路径
